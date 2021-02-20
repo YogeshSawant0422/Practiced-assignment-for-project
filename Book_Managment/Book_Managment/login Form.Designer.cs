52ccc47229cb873922be114c4b48e31f7d91ed92
Yogesh Sawant <yogeshsawant0422@gmail.com>
2021-02-20 14:47:44 +0530

    Practice Assignment

---
+++ Book_Managment/Book_Managment/login Form.Designer.cs
@@ -0,0 +1,156 @@
+﻿namespace Book_Managment
+{
+    partial class frm_Login_form
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.lbl_Login_Form = new System.Windows.Forms.Label();
+            this.lbl_Username = new System.Windows.Forms.Label();
+            this.lbl_Password = new System.Windows.Forms.Label();
+            this.txt_username = new System.Windows.Forms.TextBox();
+            this.txt_Password = new System.Windows.Forms.TextBox();
+            this.btn_Submit = new System.Windows.Forms.Button();
+            this.lbl_Note = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // lbl_Login_Form
+            // 
+            this.lbl_Login_Form.BackColor = System.Drawing.Color.LightCoral;
+            this.lbl_Login_Form.Font = new System.Drawing.Font("Cambria", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lbl_Login_Form.ForeColor = System.Drawing.Color.Cyan;
+            this.lbl_Login_Form.Location = new System.Drawing.Point(244, 9);
+            this.lbl_Login_Form.Name = "lbl_Login_Form";
+            this.lbl_Login_Form.Size = new System.Drawing.Size(144, 36);
+            this.lbl_Login_Form.TabIndex = 0;
+            this.lbl_Login_Form.Text = "Login Form";
+            this.lbl_Login_Form.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // lbl_Username
+            // 
+            this.lbl_Username.BackColor = System.Drawing.Color.MistyRose;
+            this.lbl_Username.Font = new System.Drawing.Font("Cambria", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lbl_Username.ForeColor = System.Drawing.Color.MidnightBlue;
+            this.lbl_Username.Location = new System.Drawing.Point(133, 76);
+            this.lbl_Username.Name = "lbl_Username";
+            this.lbl_Username.Size = new System.Drawing.Size(120, 44);
+            this.lbl_Username.TabIndex = 0;
+            this.lbl_Username.Text = "Username";
+            this.lbl_Username.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            this.lbl_Username.Click += new System.EventHandler(this.label2_Click);
+            // 
+            // lbl_Password
+            // 
+            this.lbl_Password.BackColor = System.Drawing.Color.MistyRose;
+            this.lbl_Password.Font = new System.Drawing.Font("Cambria", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lbl_Password.ForeColor = System.Drawing.Color.MidnightBlue;
+            this.lbl_Password.Location = new System.Drawing.Point(133, 159);
+            this.lbl_Password.Name = "lbl_Password";
+            this.lbl_Password.Size = new System.Drawing.Size(119, 42);
+            this.lbl_Password.TabIndex = 0;
+            this.lbl_Password.Text = "Password";
+            this.lbl_Password.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // txt_username
+            // 
+            this.txt_username.Font = new System.Drawing.Font("Calisto MT", 11.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txt_username.Location = new System.Drawing.Point(338, 92);
+            this.txt_username.MaxLength = 8;
+            this.txt_username.Name = "txt_username";
+            this.txt_username.Size = new System.Drawing.Size(158, 25);
+            this.txt_username.TabIndex = 1;
+            // 
+            // txt_Password
+            // 
+            this.txt_Password.Font = new System.Drawing.Font("Cambria", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txt_Password.Location = new System.Drawing.Point(338, 174);
+            this.txt_Password.MaxLength = 8;
+            this.txt_Password.Name = "txt_Password";
+            this.txt_Password.PasswordChar = '*';
+            this.txt_Password.Size = new System.Drawing.Size(158, 23);
+            this.txt_Password.TabIndex = 2;
+            // 
+            // btn_Submit
+            // 
+            this.btn_Submit.BackColor = System.Drawing.SystemColors.ScrollBar;
+            this.btn_Submit.Cursor = System.Windows.Forms.Cursors.WaitCursor;
+            this.btn_Submit.Font = new System.Drawing.Font("Cambria", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btn_Submit.ForeColor = System.Drawing.SystemColors.InactiveCaptionText;
+            this.btn_Submit.Location = new System.Drawing.Point(218, 275);
+            this.btn_Submit.Name = "btn_Submit";
+            this.btn_Submit.Size = new System.Drawing.Size(144, 45);
+            this.btn_Submit.TabIndex = 3;
+            this.btn_Submit.Text = "Submit";
+            this.btn_Submit.UseVisualStyleBackColor = false;
+            this.btn_Submit.UseWaitCursor = true;
+            this.btn_Submit.Click += new System.EventHandler(this.btn_Submit_Click);
+            // 
+            // lbl_Note
+            // 
+            this.lbl_Note.Font = new System.Drawing.Font("Cambria", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lbl_Note.ForeColor = System.Drawing.Color.SaddleBrown;
+            this.lbl_Note.Location = new System.Drawing.Point(124, 228);
+            this.lbl_Note.Name = "lbl_Note";
+            this.lbl_Note.Size = new System.Drawing.Size(436, 23);
+            this.lbl_Note.TabIndex = 4;
+            this.lbl_Note.Text = "Note";
+            this.lbl_Note.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // frm_Login_form
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.ControlDarkDark;
+            this.ClientSize = new System.Drawing.Size(661, 332);
+            this.Controls.Add(this.lbl_Note);
+            this.Controls.Add(this.btn_Submit);
+            this.Controls.Add(this.txt_Password);
+            this.Controls.Add(this.txt_username);
+            this.Controls.Add(this.lbl_Password);
+            this.Controls.Add(this.lbl_Username);
+            this.Controls.Add(this.lbl_Login_Form);
+            this.MaximizeBox = false;
+            this.Name = "frm_Login_form";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Login form";
+            this.Load += new System.EventHandler(this.Frm_Login_form_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label lbl_Login_Form;
+        private System.Windows.Forms.Label lbl_Username;
+        private System.Windows.Forms.Label lbl_Password;
+        private System.Windows.Forms.TextBox txt_username;
+        private System.Windows.Forms.TextBox txt_Password;
+        private System.Windows.Forms.Button btn_Submit;
+        private System.Windows.Forms.Label lbl_Note;
+    }
+}
+
---
+++ Book_Managment/Book_Managment/login Form.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+namespace Book_Managment
+{
+    public partial class frm_Login_form : Form
+    {
+        public frm_Login_form()
+        {
+            InitializeComponent();
+        }
+
+        private void label2_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Frm_Login_form_Load(object sender, EventArgs e)
+        {
+            lbl_Note.Text = "Please enter your username && password";
+
+        }
+
+        private void btn_Submit_Click(object sender, EventArgs e)
+        {
+            if (txt_username.Text == "yogesh" && txt_Password.Text == "y123")
+            {
+                MessageBox.Show("Login Successfull !!!!");
+
+                frm_Add_New_Book obj = new frm_Add_New_Book();
+                obj.Show();
+
+                this.Hide();
+            }
+            else
+            {
+                lbl_Note.Text = "Please enter valid details.";
+                lbl_Note.ForeColor = Color.Red;
+                   
+            }
+            txt_username.Text = "";
+            txt_Password.Text = "";
+
+            txt_username.Focus();
+
+        }
+
+        
+    }
+}
